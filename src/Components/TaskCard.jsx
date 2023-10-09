24f0358f99d7f8000e27803cff95018b836f90e2
Leonardo Cauna <leonardo.cauna@gmail.com>
2023-10-09 13:36:38 -0300

    Fixing errors

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -6,56 +6,56 @@ import { useState } from 'react'
 
 export const runtime = "edge";
 export default function TaskCard ({ element }){
-    // const [uprating, setUpRating] = useState(false)
-    // const [downrating, setDownRating] = useState(false)
+    const [uprating, setUpRating] = useState(false)
+    const [downrating, setDownRating] = useState(false)
 
-    // async function LoadElement(){
-    //     const res = await fetch(`${process.env.LOCALHOST}/api/elements/${element.id}?${Date.now()}`)
-    //     console.log(res);
-    //     return await res.json()
-    // }
+    async function LoadElement(){
+        const res = await fetch(`${process.env.LOCALHOST}/api/elements/${element.id}?${Date.now()}`)
+        console.log(res);
+        return await res.json()
+    }
 
-    // async function uploadRating(id, totalRatings){
-    //     try {
-    //         const res = await fetch('/api/elements/'+id, {
-    //         method: "PUT",
-    //         body: JSON.stringify({totalRatings}),
-    //         headers:{
-    //             'Content-Type': 'application/json'
-    //         }})
-    //         console.log(res);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    async function uploadRating(id, totalRatings){
+        try {
+            const res = await fetch('/api/elements/'+id, {
+            method: "PUT",
+            body: JSON.stringify({totalRatings}),
+            headers:{
+                'Content-Type': 'application/json'
+            }})
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    // async function upRating(value){
-    //     if(value===0){
-    //             const data = await LoadElement()
-    //             let totalRatings = data.elements.totalRatings
-    //             totalRatings++
-    //             uploadRating(data.elements.id, totalRatings)
-    //     }
-    //     else if(value===1){
-    //         const data = await LoadElement()
-    //         let totalRatings = data.elements.totalRatings
-    //         totalRatings--
-    //         uploadRating(data.elements.id, totalRatings)
-    //     }
-    // }
+    async function upRating(value){
+        if(value===0){
+                const data = await LoadElement()
+                let totalRatings = data.elements.totalRatings
+                totalRatings++
+                uploadRating(data.elements.id, totalRatings)
+        }
+        else if(value===1){
+            const data = await LoadElement()
+            let totalRatings = data.elements.totalRatings
+            totalRatings--
+            uploadRating(data.elements.id, totalRatings)
+        }
+    }
     
-    // async function downRating(value){
-    //     const data = await LoadElement()
-    //     let totalRatings = data.elements.totalRatings
-    //     if(value===0){
-    //         totalRatings++
-    //         uploadRating(data.elements.id, totalRatings)
-    //     }
-    //     else if(value===1){
-    //         totalRatings--
-    //         uploadRating(data.elements.id, totalRatings)
-    //     }
-    // }
+    async function downRating(value){
+        const data = await LoadElement()
+        let totalRatings = data.elements.totalRatings
+        if(value===0){
+            totalRatings++
+            uploadRating(data.elements.id, totalRatings)
+        }
+        else if(value===1){
+            totalRatings--
+            uploadRating(data.elements.id, totalRatings)
+        }
+    }
     return(
         <div className='bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer'>
         <h3 className='font-bold text-2xl mb-2 text-slate-300'>{element.title}</h3>
@@ -64,47 +64,47 @@ export default function TaskCard ({ element }){
         <p className='text-slate-400'>{element.rating}</p>
         <p className='text-slate-400'>{element.totalRatings}</p>
         <input type='checkbox' value="up" id={`upVote${element.id}`} 
-            // onChange={(e) => { 
-            //     setUpRating(e.target.value) 
-            //     setUpRating(!uprating) 
-            //     if(uprating === false){
-            //         localStorage.removeItem("thumbsDown")
-            //         localStorage.setItem("thumbsUp", true)
-            //         if (document.getElementById(`downVote${element.id}`).checked == true) {
-            //             document.getElementById(`downVote${element.id}`).checked = false
-            //             setUpRating(true)
-            //             setDownRating(false)
-            //         }
-            //         else upRating(0)
-            //     }
-            //     else if(uprating === true){
-            //         localStorage.removeItem("thumbsUp")
-            //         setUpRating(false)
-            //         upRating(1)
-            //     }
-            // }}
+            onChange={(e) => { 
+                setUpRating(e.target.value) 
+                setUpRating(!uprating) 
+                if(uprating === false){
+                    localStorage.removeItem("thumbsDown")
+                    localStorage.setItem("thumbsUp", true)
+                    if (document.getElementById(`downVote${element.id}`).checked == true) {
+                        document.getElementById(`downVote${element.id}`).checked = false
+                        setUpRating(true)
+                        setDownRating(false)
+                    }
+                    else upRating(0)
+                }
+                else if(uprating === true){
+                    localStorage.removeItem("thumbsUp")
+                    setUpRating(false)
+                    upRating(1)
+                }
+            }}
             >
         </input>
         <input type='checkbox' value="down" id={`downVote${element.id}`} 
-            // onChange={(e) => { 
-            //     setDownRating(e.target.value) 
-            //     setDownRating(!downrating) 
-            //     if(downrating === false){
-            //         localStorage.removeItem("thumbsUp")
-            //         localStorage.setItem("thumbsDown", true)
-            //         if (document.getElementById(`upVote${element.id}`).checked == true) {
-            //             document.getElementById(`upVote${element.id}`).checked = false
-            //             setDownRating(true)
-            //             setUpRating(false)
-            //         }
-            //         else downRating(0)
-            //     }  
-            //     else if(downrating === true){
-            //         localStorage.removeItem("thumbsDown")
-            //         setDownRating(false)
-            //         downRating(1)
-            //     }
-            // }}
+            onChange={(e) => { 
+                setDownRating(e.target.value) 
+                setDownRating(!downrating) 
+                if(downrating === false){
+                    localStorage.removeItem("thumbsUp")
+                    localStorage.setItem("thumbsDown", true)
+                    if (document.getElementById(`upVote${element.id}`).checked == true) {
+                        document.getElementById(`upVote${element.id}`).checked = false
+                        setDownRating(true)
+                        setUpRating(false)
+                    }
+                    else downRating(0)
+                }  
+                else if(downrating === true){
+                    localStorage.removeItem("thumbsDown")
+                    setDownRating(false)
+                    downRating(1)
+                }
+            }}
             >
         </input>
     </div>
