2377c479514dcdc1c80582ba2c127b9c52fe22c0
Leonardo Cauna <leonardo.cauna@gmail.com>
2023-10-09 13:34:45 -0300

    Fixing errors

--- src/Components/ShowCards.jsx
+++ src/Components/ShowCards.jsx
@@ -1,17 +1,17 @@
 import TaskCard from "./TaskCard"
 export const runtime = "edge";
-async function LoadElements(){
-    const res = await fetch(`${process.env.LOCALHOST}/api/elements?${Date.now()}`)
-    return await res.json()
-  }
+// async function LoadElements(){
+//     const res = await fetch(`${process.env.LOCALHOST}/api/elements?${Date.now()}`)
+//     return await res.json()
+//   }
 export default async function ShowCards (){
-    const data = await LoadElements()
-    const elements = data.elements
+    // const data = await LoadElements()
+    // const elements = data.elements
     return(
         <div className='grid grid-cols-3 gap-3 mt-10'>
-        {elements.map((element)=>(
+        {/* {elements.map((element)=>(
             <TaskCard element = {element} key = {element.id}/>
-        ))}
+        ))} */}
         </div>
 )}
     
