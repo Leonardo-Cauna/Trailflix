99ff684bab4d0a3dce89a2afd100d154e63fbfe8
Leonardo Cauna <leonardo.cauna@gmail.com>
2023-10-11 00:27:01 -0300

    Fixed tailwind config

--- src/Components/ShowCards.jsx
+++ src/Components/ShowCards.jsx
@@ -1,7 +1,7 @@
 import TaskCard from "./TaskCard"
 // export const runtime = "edge";
 async function LoadElements(){
-    const res = await fetch(`${process.env.OCALHOST}/api/elements?${Date.now()}`)
+    const res = await fetch(`${process.env.LOCALHOST}/api/elements?${Date.now()}`)
     console.log(process.env.LOCALHOST)
     return await res.json()
   }
